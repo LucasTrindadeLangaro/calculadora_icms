ca6b5b67679a28d19d73112a6ef3d7d7161225f1
Lucas Trindade Langaro <langarolucas@gmail.com>
2023-10-17 15:41:54 -0300

    Calculo com reducao
    Adicionado validação para o calculo com redução

--- calc_icms_pysimplegui.py
+++ calc_icms_pysimplegui.py
@@ -3,9 +3,10 @@ from PySimpleGUI import *
 layout = [
     [Text('Valor total do produto'), InputText(key='vlr_prod')],
     [Text('Aliquota do ICMS'), InputText(key='aliq')],
-    #[Text('Redução da Base de calculo'), InputText(key=)],
-    [Button('Calcular'), Button('Cancelar')],
+    [Text('Redução da Base de calculo', visible=False, key='input_texto_reducao'), InputText(key='reducao_base', visible=False)],
+    [Button('Calcular'), Button('Calcular com Redução'), Button('Cancelar')],
     [Text('Base ICMS: ' ,key='texto_base_icms')],
+    [Text('Redução de Base: ',visible=False, key='texto_reducao')],
     [Text('Aliq ICMS: ' ,key='texto_aliq_icms')],
     [Text('Valor ICMS: ',key='texto_valor_icms')]
 ]
@@ -16,16 +17,42 @@ janela = Window("Calculadora de ICMS", layout)
 while True:
     #evento = todo clique que vc faz dentro da tela
     #valores
+
     evento, valores = janela.read()
+
     if evento == WIN_CLOSED or evento == 'Cancelar':
         break
+
+    if evento == 'Calcular com Redução':
+        janela['input_texto_reducao'].update(visible=True)
+        janela['reducao_base'].update(visible=True)
+
     if evento == 'Calcular':
-        vlr_icms  = float(valores['vlr_prod'])*(float(valores['aliq'])/100)
-        janela['texto_base_icms'].update(f'Base ICMS: R$ {valores["vlr_prod"]}')
-        janela['texto_aliq_icms'].update(f'Aliq ICMS: {valores["aliq"]}%')
-        formatado_icms = f'Valor ICMS: R$ {vlr_icms:_.2f}'
-        formatado_icms = formatado_icms.replace('.',',').replace('_','.')
-        janela['texto_valor_icms'].update(formatado_icms)
-        print(vlr_icms)
+
+        if janela['reducao_base'].visible and float(valores['reducao_base']) > 0:
+            if float(valores['reducao_base']) == 0:
+                break
+            base_icms = float(valores['vlr_prod'])*(float(valores['reducao_base'])/100)
+            vlr_icms = base_icms*(float(valores['aliq'])/100)
+
+            janela['texto_base_icms'].update(f'Base ICMS: R$ {base_icms}')
+            janela['texto_reducao'].update(visible=True)
+            janela['texto_reducao'].update(f'Redução de Base: {valores["reducao_base"]}%')
+            janela['texto_aliq_icms'].update(f'Aliq ICMS: {valores["aliq"]}%')
+
+            formatado_icms = f'Valor ICMS: R$ {vlr_icms:_.2f}'
+            formatado_icms = formatado_icms.replace('.',',').replace('_','.')
+            janela['texto_valor_icms'].update(formatado_icms)
+        
+        else:
+            vlr_icms  = float(valores['vlr_prod'])*(float(valores['aliq'])/100)
+
+            janela['texto_base_icms'].update(f'Base ICMS: R$ {valores["vlr_prod"]}')
+            janela['texto_aliq_icms'].update(f'Aliq ICMS: {valores["aliq"]}%')
+
+            formatado_icms = f'Valor ICMS: R$ {vlr_icms:_.2f}'
+            formatado_icms = formatado_icms.replace('.',',').replace('_','.')
+            janela['texto_valor_icms'].update(formatado_icms)
+            print(vlr_icms)
 
 janela.close()
